02972508d5cbeeb21f655b74ae4a554366867a9c
Kirk Tolleshaug <ktolleshaug@gmail.com>
2015-09-08 17:49:42 -0500

    real fix url

--- FirstSetup.bat
+++ FirstSetup.bat
@@ -1,3 +1,3 @@
 echo "Running script..."
-powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ktoll2/autoinstall/master/Powershell/AutoInstalls/AutoInstalls/SPCStart.ps1')"
+powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ktoll2/autoinstall/master/SPCStart.ps1')"
 echo "All done."
